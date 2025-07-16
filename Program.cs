7c98ed41d0d197a1a4a42b3cd1825d749fd3fc3b
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-07-16 18:22:55 -0400

    commentary

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace Assignment2._4._3
     {
         static void Main(string[] args)
         {
-            int xCoordinate = 0;
+            int xCoordinate = 0;// Initialize xCoordinate to 0
             int yCoordinate = 0;
 
             Console.WriteLine("Enter the X coordinate:");
-            int.TryParse(Console.ReadLine(), out xCoordinate);
+            int.TryParse(Console.ReadLine(), out xCoordinate);// Try to parse the input as an integer
             Console.WriteLine("Enter the Y coordinate:");
             int.TryParse(Console.ReadLine(), out yCoordinate);
 
-            if (xCoordinate > 0 && yCoordinate > 0)
+            if (xCoordinate > 0 && yCoordinate > 0)// Check if the point is in the first quadrant
             {
                 Console.WriteLine("The coordinate point " + $"({xCoordinate}, {yCoordinate}) is in the first quadrant.");
             }
@@ -30,7 +30,7 @@ namespace Assignment2._4._3
             {
                 Console.WriteLine("The coordinate point " + $"({xCoordinate}, {yCoordinate}) is in the fourth quadrant.");
             }
-            else if (xCoordinate == 0 && yCoordinate == 0)
+            else if (xCoordinate == 0 && yCoordinate == 0) // Check if the point is at the origin
             {
                 Console.WriteLine("The coordinate point is at the origin.");
             }
