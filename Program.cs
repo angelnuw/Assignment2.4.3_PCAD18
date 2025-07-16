66e5508b1208fe1f1086423535ac0e1685326f43
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-07-16 18:21:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Design;
+
+namespace Assignment2._4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int xCoordinate = 0;
+            int yCoordinate = 0;
+
+            Console.WriteLine("Enter the X coordinate:");
+            int.TryParse(Console.ReadLine(), out xCoordinate);
+            Console.WriteLine("Enter the Y coordinate:");
+            int.TryParse(Console.ReadLine(), out yCoordinate);
+
+            if (xCoordinate > 0 && yCoordinate > 0)
+            {
+                Console.WriteLine("The coordinate point " + $"({xCoordinate}, {yCoordinate}) is in the first quadrant.");
+            }
+            else if (xCoordinate < 0 && yCoordinate > 0)
+            {
+                Console.WriteLine("The coordinate point " + $"({xCoordinate}, {yCoordinate}) is in the second quadrant.");
+            }
+            else if (xCoordinate < 0 && yCoordinate < 0) 
+            {
+                Console.WriteLine("The coordinate point " + $"({xCoordinate}, {yCoordinate}) is in the third quadrant.");
+            }
+            else if (xCoordinate > 0 && yCoordinate < 0)
+            {
+                Console.WriteLine("The coordinate point " + $"({xCoordinate}, {yCoordinate}) is in the fourth quadrant.");
+            }
+            else if (xCoordinate == 0 && yCoordinate == 0)
+            {
+                Console.WriteLine("The coordinate point is at the origin.");
+            }
+        }
+    }
+}
